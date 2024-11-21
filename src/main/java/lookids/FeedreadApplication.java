141a12370d313de7fdaa2af68034636282cd0e16
miinns <miin006@naver.com>
2024-11-21 13:24:37 +0900

    Fix: eureka 추가 KAN-235

--- src/main/java/lookids/FeedreadApplication.java
+++ src/main/java/lookids/FeedreadApplication.java
@@ -2,9 +2,10 @@ package lookids.feedread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "lookids")
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 public class FeedreadApplication {
 
 	public static void main(String[] args) {
