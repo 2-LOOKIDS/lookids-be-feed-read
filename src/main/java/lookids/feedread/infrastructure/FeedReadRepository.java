fb68e0abd8986bac11034cfc6c578b303dfdcbb4
miinns <miin006@naver.com>
2024-12-03 12:08:11 +0900

    Fix: 메인 페이지 피드 get 수정 KAN-362

--- src/main/java/lookids/feedread/infrastructure/FeedReadRepository.java
+++ src/main/java/lookids/feedread/infrastructure/FeedReadRepository.java
@@ -11,6 +11,7 @@ import lookids.feedread.domain.FeedRead;
 
 public interface FeedReadRepository extends MongoRepository<FeedRead, String> {
 	Page<FeedRead> findByUuidAndStateFalse(String uuid, Pageable pageable);
+	Page<FeedRead> findByUuidInAndStateFalse(List<String> UuidList, Pageable pageable);
 	Page<FeedRead> findByFeedCodeInAndStateFalse(List<String> targetCodeList, Pageable pageable);
 	List<FeedRead> findAllByUuid(String uuid);
 	Optional<FeedRead> findByFeedCodeAndStateFalse(String feedCode);
