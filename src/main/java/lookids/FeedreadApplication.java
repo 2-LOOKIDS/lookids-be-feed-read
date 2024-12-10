e874ca4d065123f087a42d45682ec5fb7cb5653d
miinns <miin006@naver.com>
2024-12-10 20:31:44 +0900

    Fix: timeout 설정 중 KAN-473

--- src/main/java/lookids/FeedreadApplication.java
+++ src/main/java/lookids/FeedreadApplication.java
@@ -2,10 +2,9 @@ package lookids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "lookids")
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class FeedreadApplication {
 
 	public static void main(String[] args) {
