55aba2cbbf364b385980787087c9bac5337eae37
miinns <miin006@naver.com>
2024-12-10 20:31:44 +0900

    Fix: timeout 설정 중 KAN-473

--- src/main/java/lookids/feedread/application/FeedReadServiceImpl.java
+++ src/main/java/lookids/feedread/application/FeedReadServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -28,15 +27,10 @@ import lookids.common.entity.BaseResponseStatus;
 import lookids.common.exception.BaseException;
 import lookids.feedread.domain.FeedRead;
 import lookids.feedread.dto.in.BlockKafkaDto;
-import lookids.feedread.dto.in.FeedDeleteKafkaDto;
 import lookids.feedread.dto.in.FeedKafkaDto;
 import lookids.feedread.dto.in.PetImageKafkaDto;
 import lookids.feedread.dto.in.PetKafkaDto;
-import lookids.feedread.dto.in.TargetKafkaDto;
-import lookids.feedread.dto.in.TargetRequestKafkaDto;
-import lookids.feedread.dto.in.UserImageKafkaDto;
 import lookids.feedread.dto.in.UserKafkaDto;
-import lookids.feedread.dto.in.UserNickNameKafkaDto;
 import lookids.feedread.dto.in.UuidKafkaDto;
 import lookids.feedread.dto.out.FavoriteResponseDto;
 import lookids.feedread.dto.out.FeedListResponseDto;
@@ -61,9 +55,39 @@ public class FeedReadServiceImpl implements FeedReadService {
 	private final KafkaTemplate<String, UuidKafkaDto> followKafkaTemplate;
 	private final KafkaTemplate<String, UuidKafkaDto> blockKafkaTemplate;
 	private final KafkaTemplate<String, PetKafkaDto> petKafkaTemplate;
-	private final KafkaTemplate<String, TargetRequestKafkaDto> recommendKafkaTemplate;
 	private final FeedReadRepository feedReadRepository;
 	private final MongoTemplate mongoTemplate;
+	private final FeedKafkaListener feedKafkaListener;
+
+	@KafkaListener(topics = "favorite-response", groupId = "feed-read-group", containerFactory = "favoriteEventListenerContainerFactory")
+	public void readFeedFavorite(FavoriteResponseDto favoriteResponseDto) {
+		String uuid = favoriteResponseDto.getUuid();
+		CompletableFuture<FavoriteResponseDto> futureFeedCodeList = favoriteEventFutureMap.get(uuid);
+		futureFeedCodeList.complete(favoriteResponseDto);
+	}
+
+	@KafkaListener(topics = "block-response", groupId = "feed-read-group", containerFactory = "blockEventListenerContainerFactory")
+	public void readBlockUuid(BlockKafkaDto blockKafkaDto) {
+		String uuid = blockKafkaDto.getUuid();
+		CompletableFuture<BlockKafkaDto> futureBlockList = blockEventFutureMap.get(uuid);
+		futureBlockList.complete(blockKafkaDto);
+	}
+
+	@KafkaListener(topics = "follow-response", groupId = "feed-read-group", containerFactory = "followEventListenerContainerFactory")
+	public void readFeedFollow(FollowResponseDto followResponseDto) {
+		String uuid = followResponseDto.getUuid();
+		CompletableFuture<FollowResponseDto> futureUuidList = followEventFutureMap.get(uuid);
+		futureUuidList.complete(followResponseDto);
+	}
+
+	@KafkaListener(topics = "petprofile-response", groupId = "feed-read-group", containerFactory = "petEventListenerContainerFactory")
+	public void readPetImage(PetImageKafkaDto petImageKafkaDto) {
+		String petCode = petImageKafkaDto.getPetCode();
+		CompletableFuture<PetImageKafkaDto> futurePetImage = petEventFutureMap.get(petCode);
+		if (futurePetImage != null) {
+			futurePetImage.complete(petImageKafkaDto);
+		}
+	}
 
 	@Override
 	public Page<FeedReadResponseDto> readFeedFavoriteList(String uuid, int page, int size) {
@@ -178,7 +202,7 @@ public class FeedReadServiceImpl implements FeedReadService {
 	public Page<FeedListResponseDto> readFeedRandomList(int page, int size) {
 		Aggregation aggregation = Aggregation.newAggregation(
 			Aggregation.match(Criteria.where("state").is(true)),
-			Aggregation.sample(size),
+		Aggregation.sample(size),
 			Aggregation.skip((long) page * size),
 			Aggregation.limit(size));
 		List<FeedRead> feedReadList = mongoTemplate.aggregate(aggregation, "feedRead", FeedRead.class).getMappedResults();
@@ -272,100 +296,5 @@ public class FeedReadServiceImpl implements FeedReadService {
 				return null;
 			});
 		}
-	}
-
-	@KafkaListener(topics = "favorite-response", groupId = "feed-read-group", containerFactory = "favoriteEventListenerContainerFactory")
-	public void readFeedFavorite(FavoriteResponseDto favoriteResponseDto) {
-		String uuid = favoriteResponseDto.getUuid();
-		CompletableFuture<FavoriteResponseDto> futureFeedCodeList = favoriteEventFutureMap.get(uuid);
-		futureFeedCodeList.complete(favoriteResponseDto);
-	}
-
-	@KafkaListener(topics = "block-response", groupId = "feed-read-group", containerFactory = "blockEventListenerContainerFactory")
-	public void readBlockUuid(BlockKafkaDto blockKafkaDto) {
-		String uuid = blockKafkaDto.getUuid();
-		CompletableFuture<BlockKafkaDto> futureBlockList = blockEventFutureMap.get(uuid);
-		futureBlockList.complete(blockKafkaDto);
-	}
-
-	@KafkaListener(topics = "follow-response", groupId = "feed-read-group", containerFactory = "followEventListenerContainerFactory")
-	public void readFeedFollow(FollowResponseDto followResponseDto) {
-		String uuid = followResponseDto.getUuid();
-		CompletableFuture<FollowResponseDto> futureUuidList = followEventFutureMap.get(uuid);
-		futureUuidList.complete(followResponseDto);
-	}
-
-	@KafkaListener(topics = "petprofile-response", groupId = "feed-read-group", containerFactory = "petEventListenerContainerFactory")
-	public void readPetImage(PetImageKafkaDto petImageKafkaDto) {
-		String petCode = petImageKafkaDto.getPetCode();
-		CompletableFuture<PetImageKafkaDto> futurePetImage = petEventFutureMap.get(petCode);
-		if (futurePetImage != null) {
-			futurePetImage.complete(petImageKafkaDto);
-		}
-	}
-	@Transactional
-	@KafkaListener(topics = "userprofile-nickname-update", groupId = "feed-read-group", containerFactory = "userNickNameEventListenerContainerFactory")
-	public void NickNameUpdateConsume(UserNickNameKafkaDto userNickNameKafkaDto) {
-		List<FeedRead> findUuid = feedReadRepository.findAllByUuid(userNickNameKafkaDto.getUuid());
-		if (findUuid.isEmpty()) {
-			throw new BaseException(BaseResponseStatus.NO_EXIST_FEED);
-		}
-		List<FeedRead> nickNameUpdate = findUuid.stream().map(userNickNameKafkaDto::toNickNameUpdate)
-			.collect(Collectors.toList());
-		feedReadRepository.saveAll(nickNameUpdate);
-	}
-
-	@Transactional
-	@KafkaListener(topics = "userprofile-image-update", groupId = "feed-read-group", containerFactory = "userProfileEventListenerContainerFactory")
-	public void ImageUpdateConsume(UserImageKafkaDto userImageKafkaDto) {
-		List<FeedRead> findUuid = feedReadRepository.findAllByUuid(userImageKafkaDto.getUuid());
-		if (findUuid.isEmpty()) {
-			throw new BaseException(BaseResponseStatus.NO_EXIST_FEED);
-		}
-		List<FeedRead> ImageUpdate = findUuid.stream().map(userImageKafkaDto::toImageUpdate)
-			.collect(Collectors.toList());
-		feedReadRepository.saveAll(ImageUpdate);
-	}
-
-	@KafkaListener(topics = "feed-delete", groupId = "feed-read-group", containerFactory = "deleteEventListenerContainerFactory")
-	public void FeedDeleteConsume(FeedDeleteKafkaDto feedDeleteKafkaDto) {
-		FeedRead feedRead = feedReadRepository.findByFeedCodeAndStateTrue(feedDeleteKafkaDto.getFeedCode())
-			.orElseThrow(() -> new BaseException(BaseResponseStatus.NO_EXIST_FEED));
-		FeedRead updatedFeedRead = feedDeleteKafkaDto.toUpdatedEntity(feedRead);
-		log.info("adskjfhaksljdfh: {}", feedDeleteKafkaDto);
-		feedReadRepository.save(updatedFeedRead);
-	}
-
-	@KafkaListener(topics = "recommend-user", groupId = "feed-read-group", containerFactory = "recommendEventListenerContainerFactory")
-	public void recommendTarget(TargetKafkaDto targetKafkaDto) {
-		List<FeedRead> findUuidList = feedReadRepository.findByFeedCodeIn(targetKafkaDto.getTargetCode());
-		List<String> uuidList = findUuidList.stream()
-			.map(FeedRead::getUuid)
-			.collect(Collectors.toList());
-		TargetRequestKafkaDto targetRequestKafkaDto = TargetRequestKafkaDto.toDto(targetKafkaDto.getAuthorUuid(), uuidList);
-		recommendKafkaTemplate.send("recommend-user-response", targetRequestKafkaDto);
-	}
-
-	@Transactional
-	@KafkaListener(topics = "petprofile-update", groupId = "feed-read-group", containerFactory = "petProfileEventListenerContainerFactory")
-	public void ImageUpdateConsume(PetImageKafkaDto petImageKafkaDto) {
-		List<FeedRead> findPetCode = feedReadRepository.findAllBypetCode(petImageKafkaDto.getPetCode());
-		if (findPetCode.isEmpty()) {
-			throw new BaseException(BaseResponseStatus.NO_EXIST_FEED);
-		}
-		List<FeedRead> ImageUpdate = findPetCode.stream().map(petImageKafkaDto::toImageUpdate)
-			.collect(Collectors.toList());
-		feedReadRepository.saveAll(ImageUpdate);
-	}
-
-	@KafkaListener(topics = "account-delete", groupId = "feed-read-group", containerFactory = "accountDeleteEventListenerContainerFactory")
-	public void ImageUpdateConsume(UuidKafkaDto uuidKafkaDto) {
-		List<FeedRead> findUuid = feedReadRepository.findAllByUuid(uuidKafkaDto.getUuid());
-		if (findUuid.isEmpty()) {
-			throw new BaseException(BaseResponseStatus.NO_EXIST_FEED);
-		}
-		List<FeedRead> feedDelete = findUuid.stream().map(uuidKafkaDto::toDelete)
-			.collect(Collectors.toList());
-		feedReadRepository.saveAll(feedDelete);
 	}
 }
